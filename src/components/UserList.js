942a15a381a35c684ac01d7e944b364f0406c79f
vich1981 <git@github.com>
2024-07-27 01:15:02 +0300

    Добавлен UserListItem и навигация Next, Previos в UserList

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,7 @@
 import React from "react";
 import * as apiCalls from '../api/apiCalls';
+import UserListItem from './UserListItem';
+import Badge from "react-bootstrap/Badge";
 
 class UserList extends React.Component {
     state = {
@@ -11,15 +13,25 @@ class UserList extends React.Component {
     };
 
     componentDidMount() {
+        this.loadData();
+    };
+
+    loadData = (requestPage = 0) => {
         apiCalls
-            .listUsers({ page: this.state.page.number, size: this.state.page.size })
+            .listUsers({ page: requestPage, size: this.state.page.size })
             .then((response) => {
                 this.setState({
                     page: response.data
                 });
-            })
-            .catch(() => {});
-    }
+            });
+    };
+
+    onClickNext = () => {
+        this.loadData(this.state.page.number + 1);
+    };
+    onClickPrevios = () => {
+        this.loadData(this.state.page.number - 1);
+    };
 
     render() {
         return (
@@ -27,19 +39,34 @@ class UserList extends React.Component {
                 <h3 className="card-title m-auto">Users</h3>
                 <div className="list-group list-group-flush" data-testid="usergroup">
                     {this.state.page.content.map((user) => {
-                        return (
-                            <div 
-                                key={user.username}
-                                className="list-group-item list-group-item-action"
-                            >
-                                {`${user.displayName}@${user.username}`}
-                            </div>
-                        );
+                        return <UserListItem key={user.username} user={user}/>;
                     })}
+                </div>
+                <div>
+                    {!this.state.page.first && (
+                        <span className="float-start">
+                            <Badge bg="light" text="dark" float="center"
+                                onClick={this.onClickPrevios}
+                                style={{cursor: 'pointer'}}
+                            >
+                                {'<'} Previos
+                            </Badge>
+                        </span>
+                    )}
+                    {!this.state.page.last && (
+                        <span className="float-end">
+                            <Badge bg="light" text="dark" float="center"
+                                onClick={this.onClickNext}
+                                style={{cursor: 'pointer'}}
+                            >
+                                Next {'>'}
+                            </Badge>
+                        </span>
+                    )}
                 </div>
             </div>
         );
-    }
+    };
 }
 
 export default UserList; 
--- src/components/UserList.spec.js
+++ src/components/UserList.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor, waitForDomChange} from '@testing-library/react';
+import { render, fireEvent, waitForDomChange} from '@testing-library/react';
 import UserList from './UserList';
 import * as apiCalls from '../api/apiCalls';
 
@@ -48,6 +48,48 @@ const mockSuccessGetSinglePage = {
         totalPages: 1
     }
 };
+const mockSuccessGetMultiPageFirst = {
+    data: {
+        content: [
+            {
+                username: 'user1',
+                displayName:'display1',
+                image: ''
+            },
+            {
+                username: 'user2',
+                displayName:'display2',
+                image: ''
+            },
+            {
+                username: 'user3',
+                displayName:'display3',
+                image: ''
+            }
+        ],
+        number: 0,
+        first: true,
+        last: false,
+        size: 3,
+        totalPages: 2
+    }
+};
+const mockSuccessGetMultiPageLast = {
+    data: {
+        content: [
+            {
+                username: 'user4',
+                displayName:'display4',
+                image: ''
+            }
+        ],
+        number: 1,
+        first: false,
+        last: true,
+        size: 3,
+        totalPages: 2
+    }
+};
 
 describe('UserList', () => {
     describe('Layout', () => {
@@ -73,6 +115,42 @@ describe('UserList', () => {
             const firstUser = await findByText('display1@user1');
             expect(firstUser).toBeInTheDocument();
         });
+        it('displays the next button when response has last value as false', async() =>{
+            apiCalls.listUsers = jest
+                .fn()
+                .mockResolvedValue(mockSuccessGetMultiPageFirst);
+            
+            const { findByText } = setup();
+            const nextLink = await findByText('Next >');
+            expect(nextLink).toBeInTheDocument();
+        });
+        it('hides the next button when response has last value as true', async() =>{
+            apiCalls.listUsers = jest
+                .fn()
+                .mockResolvedValue(mockSuccessGetMultiPageLast);
+            
+            const { findByText } = setup();
+            const nextLink = await findByText('Next >');
+            expect(nextLink).not.toBeInTheDocument();
+        });
+        it('displays the previos button when response has first value as false', async() =>{
+            apiCalls.listUsers = jest
+                .fn()
+                .mockResolvedValue(mockSuccessGetMultiPageLast);
+            
+            const { findByText } = setup();
+            const previos = await findByText('< Previos');
+            expect(previos).toBeInTheDocument();
+        });
+        it('hides the previos button when response has first value as true', async() =>{
+            apiCalls.listUsers = jest
+                .fn()
+                .mockResolvedValue(mockSuccessGetMultiPageFirst);
+            
+            const { findByText } = setup();
+            const previos = await findByText('< Previos');
+            expect(previos).not.toBeInTheDocument();
+        });
     });
     describe('Lifecycle', () => {
         it('calls listUsers api when it is rendered', () =>{
@@ -88,6 +166,32 @@ describe('UserList', () => {
                 .mockResolvedValue(mockedEmptySuccessResponse);
             setup();
             expect(apiCalls.listUsers).toHaveBeenCalledWith({ page: 0, size: 3 });
+        });
+    });
+    describe('Interactions', () => {
+        it('loads next page when clicked to next button', async () => {
+            apiCalls.listUsers = jest
+                .fn()
+                .mockResolvedValueOnce(mockSuccessGetMultiPageFirst)
+                .mockResolvedValueOnce(mockSuccessGetMultiPageLast);
+            
+            const { findByText } = setup();
+            const nextLink = await findByText('Next >');
+            fireEvent.click(nextLink);
+            const secondPageUser = await findByText('display4@user4');
+            expect(secondPageUser).toBeInTheDocument();
+        });
+        it('loads previos page when clicked to previos button', async () => {
+            apiCalls.listUsers = jest
+                .fn()
+                .mockResolvedValueOnce(mockSuccessGetMultiPageLast)
+                .mockResolvedValueOnce(mockSuccessGetMultiPageFirst);
+            
+            const { findByText } = setup();
+            const previos = await findByText('< Previos');
+            fireEvent.click(previos);
+            const firstPageUser = await findByText('display1@user1');
+            expect(firstPageUser).toBeInTheDocument();
         });
     });
 });
